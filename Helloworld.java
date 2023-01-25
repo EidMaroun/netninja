a3838ce64eafb6e4cafdfca0c7725b10e55a7861
Maroun Eid <marouneid130@gmail.com>
2023-01-25 17:10:02 +0200

    added and printed the int y

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Helloworld!");
+		System.out.println(123_456_789);
+		int y = 10;
+		int x = y/2;
+		System.out.println(y);
+		System.out.println(x);
+
+	}
+}
